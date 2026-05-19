d88570762ef798cecb9b6ed1134ee426b15d1287
csarrr8alcarazzz <cesaralcaraz824@gmail.com>
2026-05-19 20:46:28 -0300

    feat: setup frontend con Vite, React y Tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-indigo-600 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">
+        Ecolingo funciona 🎉
+      </h1>
+    </div>
+  )
+}
+
+export default App
